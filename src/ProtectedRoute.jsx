7ee903f0c8228b0209df906270a0f0838a77bb9b
furkanalsancak1 <71776864+furkanalsancak1@users.noreply.github.com>
2025-01-04 20:40:10 +0000

    Delete google-cloud-key.json

--- src/ProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-function ProtectedRoute({ isAuthenticated, children }) {
-    return isAuthenticated ? children : <Navigate to="/login" />;
-}
-
-export default ProtectedRoute;
